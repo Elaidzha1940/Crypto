711c0bec47f760f116856f17c633dd66fa3e01b0
Elaidzha1940 <eli080293@gmail.com>
2023-05-14 18:59:59 +0700

    Crypto

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Elaidzha Shchukin on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
