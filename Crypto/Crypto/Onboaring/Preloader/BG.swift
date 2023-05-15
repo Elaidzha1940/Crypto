dec8838205ee4aa864537004fc7838aceccde506
Elaidzha1940 <eli080293@gmail.com>
2023-05-15 15:07:31 +0700

    Crypto + Fixed BG

--- Crypto/Crypto/Onboaring/Preloader/BG.swift
+++ Crypto/Crypto/Onboaring/Preloader/BG.swift
@@ -16,15 +16,27 @@ struct BG: View {
         
         ZStack {
             
+            LinearGradient(colors: [Color(toElement: .bgmain) .opacity(0.7)], startPoint: .topTrailing, endPoint: .bottomLeading)
+                .ignoresSafeArea()
+            
+            VStack {
+                Circle()
+                    .frame(width: 123, height: 123)
+                    .foregroundColor(Color(toElement: .progress))
+                    .blur(radius: 80)
+                    .offset(x: 100, y: -150)
+                
+                Circle()
+                    .frame(width: 123, height: 123)
+                    .foregroundColor(Color(toElement: .bgred))
+                    .blur(radius: 80)
+                    .offset(x: -120, y: 130)
+            }
+            
+            ProgressBar()
+            
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(RadialGradient(gradient: Gradient(colors: [Color(toElement: .progress), Color(toElement: .bgred)]),
-                                   center: .topTrailing, startRadius: 55, endRadius: 900))
-        
-        
-        //.background(Color(toElement: .bgmain))
     }
-        
 }
 
 struct BG_Previews: PreviewProvider {
