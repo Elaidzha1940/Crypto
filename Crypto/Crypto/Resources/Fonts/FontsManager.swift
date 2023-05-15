edae8b3665241468176370fde155a1027e40b6b2
Elaidzha1940 <eli080293@gmail.com>
2023-05-15 17:09:38 +0700

    Crypto + Fixinf Top + added Fonts

---
+++ Crypto/Crypto/Resources/Fonts/FontsManager.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: Crypto
+//  File: FontsManager.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.05.2023
+//
+//
+//
+//  */
+
+import SwiftUI
+
+struct Fonts {
+    struct Inter {
+        static let regular = "Inter-Regular"
+        static let medium = "Inter-Medium"
+        static let bold = "Inter-Bold"
+    }
+}
+
+//    .font(custom(Fonts.Inter.regular, size: 17))
